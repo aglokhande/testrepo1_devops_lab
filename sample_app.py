8f814325126e28f5516fb9a241a0f29cb3cccf53
advait-lokhande <advait.g.l@gmail.com>
2025-01-27 23:52:24 +0530

    adding sample_app.py source code

---
+++ sample_app.py
@@ -0,0 +1,11 @@
+# add_two_numbers.py
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {result}.")
